710aab42bfc9f49b463cc730491e6af10eb21b49
Fikr Abebe <fikrabebe24@gmail.com>
2024-04-22 22:38:53 -0500

    Dapper Exercise 2

---
+++ DapExc1/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DapExc1
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+		void CreateProduct(string name, double price, int categoryID);
+	}
+}
+
---
+++ DapExc1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DapExc1
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+
+		public int ProductID { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int CategoryID { get; set; }
+		public int OnSale { get; set; }
+		public string StockLeve { get; set; }
+	}
+}
+
